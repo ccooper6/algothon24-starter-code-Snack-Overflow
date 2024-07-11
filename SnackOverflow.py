51b31e4a4286f30d5168df238f487be1b6e4c0f7
Caleb <cco139@uclive.ac.nz>
2024-07-11 17:21:50 +0800

    Trying a linear regression approach

--- SnackOverflow.py
+++ SnackOverflow.py
@@ -1,23 +1,37 @@
-
 import numpy as np
-
-##### TODO #########################################
-### RENAME THIS FILE TO YOUR TEAM NAME #############
-### IMPLEMENT 'getMyPosition' FUNCTION #############
-### TO RUN, RUN 'eval.py' ##########################
+from sklearn.linear_model import LinearRegression
 
 nInst = 50
 currentPos = np.zeros(nInst)
-
 
 def getMyPosition(prcSoFar):
     global currentPos
     (nins, nt) = prcSoFar.shape
     if (nt < 2):
         return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
+
+    # Linear regression
+    predictions = np.zeros(nins)
+    for i in range(nins):
+        X = np.arange(max(nt-35, 0), nt).reshape(-1, 1)
+        y = prcSoFar[i, max(nt-35, 0):nt]
+        if len(y) < 2:
+            predictions[i] = prcSoFar[i, -1]
+        else:
+            model = LinearRegression()
+            model.fit(X, y)
+            next_time_step = np.array([[nt]])
+            predictions[i] = model.predict(next_time_step)[0]
+
+    lastRet = np.log(predictions / prcSoFar[:, -1])
     lNorm = np.sqrt(lastRet.dot(lastRet))
     lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
-    currentPos = np.array([int(x) for x in currentPos+rpos])
+
+    rpos = np.array([int(5000 * x / prcSoFar[i, -1]) for i, x in enumerate(lastRet)])
+
+    buy_positions = np.array([x if x > 0 else 0 for x in rpos])
+    sell_positions = np.array([x if x < 0 else 0 for x in rpos])
+
+    currentPos = np.array([int(x) for x in currentPos + buy_positions + sell_positions])
+
     return currentPos
